8ecae5b2fd0b2b8c31324ed81a0a84652dcaac93
Luke Childs <lukechilds123@gmail.com>
2016-04-14 21:57:28 +0100

    Add tests for text method

--- test/unit.js
+++ test/unit.js
@@ -9,6 +9,21 @@ describe('AtHash', function() {
     expect(AtHash()).to.be.an.instanceof(Parser);
   });
 
+  describe('.text()', function() {
+
+    it('Should set text property', function () {
+      var atHash = AtHash();
+      expect(atHash._text).to.equal(null);
+      atHash.text('foo');
+      expect(atHash._text).to.equal('foo');
+    });
+
+    it('Should be chainable', function () {
+      expect(AtHash().text()).to.be.an.instanceof(Parser);
+    });
+
+  });
+
   describe('.parse()', function() {
 
     it('Empty instance should return null', function () {
@@ -18,5 +33,7 @@ describe('AtHash', function() {
     it('Instance wirh text should return text', function () {
       expect(AtHash('foo').parse()).to.equal('foo');
     });
+
   });
+
 });
