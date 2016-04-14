a15b5cd9dfe3c7c01d381f9c104226d568e6af68
Luke Childs <lukechilds123@gmail.com>
2016-04-14 21:41:17 +0100

    Add more tests

--- test/unit.js
+++ test/unit.js
@@ -4,7 +4,19 @@ var AtHash = require('../dist/athash');
 var Parser = require('../dist/parser');
 
 describe('AtHash', function() {
+
   it('should return instance of parser', function () {
     expect(AtHash()).to.be.an.instanceof(Parser);
+  });
+
+  describe('.parse()', function() {
+
+    it('Empty instance should return null', function () {
+      expect(AtHash().parse()).to.equal(null);
+    });
+
+    it('Instance wirh text should return text', function () {
+      expect(AtHash('foo').parse()).to.equal('foo');
+    });
   });
 });
