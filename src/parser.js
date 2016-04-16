673e9b23cdbb9aa1bf04fa5ba6204f9f4fd7bd47
Luke Childs <lukechilds123@gmail.com>
2016-04-16 16:07:10 +0100

    Add default hashtag filters to parser

--- src/parser.js
+++ src/parser.js
@@ -2,6 +2,11 @@ export default class Parser {
 
   constructor(text = null) {
     this.text(text);
+    this.filters = {
+      hashtags: {
+        regex: /\B#\w*[a-zA-Z]+\w*/g
+      }
+    };
   }
 
   // Set text to parse
