9706608aa791af56aa71e13c02e3956bff7298b6
Luke Childs <lukechilds123@gmail.com>
2016-04-20 21:13:10 +0100

    Add twitter filter for mentions

--- src/filters/twitter.js
+++ src/filters/twitter.js
@@ -1,5 +1,8 @@
 export default {
   hashtags: {
     filter: tag => `<a target="_blank" href="https://twitter.com/hashtag/${tag.replace('#', '')}">${tag}</a>`
+  },
+  mentions: {
+    filter: user => `<a target="_blank" href="https://twitter.com/${user.replace('@', '')}">${user}</a>`
   }
 };
